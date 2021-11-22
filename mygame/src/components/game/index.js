7ba3d25d8f7ae88136831285bb77c67a34752e3d
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-11-22 09:13:48 +0300

    sd

--- mygame/src/components/game/index.js
+++ mygame/src/components/game/index.js
@@ -4,11 +4,8 @@ import imgcu from "../.././img/g.jpg"
 
 export default function Game() {
     let qush = [{img:imgcu, answer:true},{img:imgcu, answer:false},{img:imgcu, answer:true}]
-    // console.log(qush[1].img);
-//  const imgArray = qush.map(()=>{
-//      return qush.img
-//  })
-// console.log(imgArray);
+
+
 
 const check = (ans,button)=>{
     if(ans==button){
@@ -19,6 +16,9 @@ const check = (ans,button)=>{
     }
 }
 
+ 
+
+
     return (
         <div>
            <div>
@@ -26,8 +26,9 @@ const check = (ans,button)=>{
                <p>SECONDS: 00</p>
                <p>POINTS 0</p>
           </div>
-               {/* <img src={imgcu} alt="" /> */}
+              
               <img src={qush[0].img} alt="" />
+       
 
               <div>
                <button onClick={()=>{check(qush[0].answer,true)}}>TRUE</button>
