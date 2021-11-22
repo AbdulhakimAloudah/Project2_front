b3310cfb390fe6131155bc3aa5c79375835b0cb8
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-11-22 16:32:18 +0300

    sfg

--- mygame/src/components/home/index.js
+++ mygame/src/components/home/index.js
@@ -2,10 +2,15 @@ import React from 'react'
 
 const Home = () => {
     return (
+      <div>
+        <h1>Find the cat</h1>
         <div>
-            <h1>home home</h1>
+          <button>Play Game</button>
+          <button>Sign Up</button>
+          <p>By clicking Sign Up, you agree to our Terms, Data Policy</p>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Home
