ce3cd277e9f348d760878731506d4f78eb4e188c
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-11-22 09:34:59 +0300

    sd

--- mygame/src/components/game/index.js
+++ mygame/src/components/game/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import imgcu from "../.././img/g.jpg"
-
+import a from "../.././img/a.jpeg"
+import b from "../.././img/b.png"
+import { useState } from 'react'
 
 export default function Game() {
-    let qush = [{img:imgcu, answer:true},{img:imgcu, answer:false},{img:imgcu, answer:true}]
+    let qush = [{img:imgcu, answer:true},{img:a, answer:false},{img:b, answer:true}]
 
 
+const [cont, setcont] = useState(0)
 
 const check = (ans,button)=>{
     if(ans==button){
@@ -14,6 +17,7 @@ const check = (ans,button)=>{
     console.log('wrong answer');
 
     }
+    setcont(cont + 1 )
 }
 
  
@@ -27,12 +31,12 @@ const check = (ans,button)=>{
                <p>POINTS 0</p>
           </div>
               
-              <img src={qush[0].img} alt="" />
+              <img src={qush[cont].img} alt="" />
        
 
               <div>
-               <button onClick={()=>{check(qush[0].answer,true)}}>TRUE</button>
-               <button onClick={()=>{check(qush[0].answer,false)}}>FALSE</button>
+               <button onClick={()=>{check(qush[cont].answer,true)}}>TRUE</button>
+               <button onClick={()=>{check(qush[cont].answer,false)}}>FALSE</button>
               </div>
         </div>
     )
