f508c04ebb19b793e862d9689ff37b0c5f3c7d17
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-11-24 10:23:36 +0300

    fgf

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,17 +1,40 @@
 import React from "react";
-
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios"
 const Login = () => {
+    const navigate = useNavigate();
+
+  const goLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const resp = await axios
+        .post("http://localhost:5000/login", {
+          email: e.target.email.value,
+          password: e.target.password.value,
+        })
+        .then((response) => {
+          console.log(response.data);
+          if (response.data.email) {
+            console.log("login successfully");
+            localStorage.setItem("User", response.data);
+            navigate("/");
+          }
+        });
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <div>
       <h1>hi king login</h1>
-      <form action="">
+      <form onSubmit={goLogin}>
         <label htmlFor="">email</label>
-        <input type="email" />
+        <input type="email" name="email" />
 
         <label htmlFor="">password</label>
-        <input type="password" />
+        <input type="password" name="password" />
 
-        <button>login</button>
+        <button type="submit">login</button>
       </form>
     </div>
   );
