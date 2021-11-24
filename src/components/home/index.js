2fa8464b0f26f3169e7cf97af136a950536ea487
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-11-24 14:11:20 +0300

    dfb

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,33 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import "./style.css";
 
 const Home = () => {
+  const [show, setshow] = useState("");
+  const [hide, sethide] = useState("hide");
+  useEffect(() => {
+    if (localStorage.getItem("User")) {
+      sethide("");
+      setshow("hide");
+    }
+  }, []);
+
   return (
     <div>
       <h1>chuse the game</h1>
       <div>
         <Link to="/cat">
-          {" "}
           <button> Find the cat</button>{" "}
         </Link>
         <Link to="/dog">
-          {" "}
           <button> Find the doog</button>{" "}
         </Link>
         <Link to="/apple">
-          {" "}
           <button> Find the apple</button>{" "}
         </Link>
         <br />
-        <Link to="/signup">
-          {" "}
+        <Link to="/signup" className={`${show}`}>
           <button>Sign Up</button>{" "}
         </Link>
 
-        <Link to="/login">
-          {" "}
+        <Link to="/login" className={`${show}`}>
           <button>login</button>{" "}
         </Link>
+        <button className={`${hide}`}>logout</button>
         <p>By clicking Sign Up, you agree to our Terms, Data Policy</p>
       </div>
     </div>
